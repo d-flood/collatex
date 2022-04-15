a2a69d4761b31d063798ea5c15a87f3b6ed23663
David Flood <davidfloodii@gmail.com>
2022-04-15 15:25:05 -0500

    remove unused imports

--- collatex-pythonport/collatex/display_module.py
+++ collatex-pythonport/collatex/display_module.py
@@ -4,12 +4,10 @@ Created on Nov 13, 2014
 @author: Ronald Haentjens Dekker
 @author: David J. Birnbaum
 '''
-from collections import defaultdict
 from textwrap import fill
 from collatex.HTML import Table, TableRow, TableCell
-from collatex.core_classes import create_table_visualization, VariantGraphRanking
+from collatex.core_classes import create_table_visualization
 import collatex.core_classes as cc
-import re
 import csv
 import io
 
@@ -103,61 +101,62 @@ def display_alignment_table_as_csv(at, output):
     return data.getvalue()
 
 
+'''Removing all graphviz support for now'''
 # visualize the variant graph into SVG format
-def display_variant_graph_as_svg(graph, output, graph_type):
-    a = graphviz.Digraph(format=graph_type, graph_attr={'rankdir': 'LR'})
-    counter = 0
-    mapping = {}
-    ranking = VariantGraphRanking.of(graph)
+# def display_variant_graph_as_svg(graph, output, graph_type):
+#     a = graphviz.Digraph(format=graph_type, graph_attr={'rankdir': 'LR'})
+#     counter = 0
+#     mapping = {}
+#     ranking = VariantGraphRanking.of(graph)
 
-    # add nodes
-    for n in graph.graph.nodes():
-        counter += 1
-        mapping[n] = str(counter)
-        if output == "svg_simple":
-            label = n.label
-            if label == '':
-                label = '#'
-            a.node(mapping[n], label=label)
-        else:
-            rank = ranking.byVertex[n]
-            readings = ["<TR><TD ALIGN='LEFT'><B>" + n.label + "</B></TD><TD ALIGN='LEFT'>exact: " + str(
-                rank) + "</TD></TR>"]
-            reverse_dict = defaultdict(list)
-            for key, value in n.tokens.items():
-                reverse_dict["".join(
-                    re.sub(r'>', r'&gt;', re.sub(r'<', r'&lt;', item.token_data["t"])) for item in value)].append(
-                    key)
-            for key, value in sorted(reverse_dict.items()):
-                reading = (
-                    "<TR><TD ALIGN='LEFT'><FONT FACE='Bukyvede'>{}</FONT></TD><TD ALIGN='LEFT'>{}</TD></TR>").format(
-                    key, ', '.join(value))
-                readings.append(reading)
-            a.node(mapping[n], label='<<TABLE CELLSPACING="0">' + "".join(readings) + '</TABLE>>')
+#     # add nodes
+#     for n in graph.graph.nodes():
+#         counter += 1
+#         mapping[n] = str(counter)
+#         if output == "svg_simple":
+#             label = n.label
+#             if label == '':
+#                 label = '#'
+#             a.node(mapping[n], label=label)
+#         else:
+#             rank = ranking.byVertex[n]
+#             readings = ["<TR><TD ALIGN='LEFT'><B>" + n.label + "</B></TD><TD ALIGN='LEFT'>exact: " + str(
+#                 rank) + "</TD></TR>"]
+#             reverse_dict = defaultdict(list)
+#             for key, value in n.tokens.items():
+#                 reverse_dict["".join(
+#                     re.sub(r'>', r'&gt;', re.sub(r'<', r'&lt;', item.token_data["t"])) for item in value)].append(
+#                     key)
+#             for key, value in sorted(reverse_dict.items()):
+#                 reading = (
+#                     "<TR><TD ALIGN='LEFT'><FONT FACE='Bukyvede'>{}</FONT></TD><TD ALIGN='LEFT'>{}</TD></TR>").format(
+#                     key, ', '.join(value))
+#                 readings.append(reading)
+#             a.node(mapping[n], label='<<TABLE CELLSPACING="0">' + "".join(readings) + '</TABLE>>')
 
     # add regular (token sequence) edges
-    for u,v,edgedata in graph.graph.edges(data=True):
-        # print('regular edges ', u, v, edgedata)
-        label = edgedata['label']
-        a.edge(mapping[u], mapping[v], label=label)
+    # for u,v,edgedata in graph.graph.edges(data=True):
+    #     # print('regular edges ', u, v, edgedata)
+    #     label = edgedata['label']
+    #     a.edge(mapping[u], mapping[v], label=label)
 
-    # add near-match edges
-    # TODO: Show all near edges (currently), or just the top one?
-    for u,v,edgedata in graph.near_graph.edges(data=True):
-        # print('near-match edges ', u, v, edgedata)
-        label = str('{:3.2f}'.format(edgedata['weight']))
-        a.edge(mapping[u], mapping[v], style='dashed', label=label)
-    # Add rank='same' information
-    for key, value in ranking.byRank.items():
-        # print(key, value)
-        # print(key, value, len(value))
-        # print(key, set(value), len(set(value)))
-        tmp = graphviz.Digraph(graph_attr={'rank': 'same'})
-        for n in [mapping[item] for item in value]:
-            tmp.node(n)
-        a.subgraph(tmp)
-    # diagnostic, not for production
-    # dot = a.draw(prog='dot')
-    # print(dot.decode(encoding='utf-8'))
-    # # display using the IPython SVG module
-    return a.render()
+    # # add near-match edges
+    # # TODO: Show all near edges (currently), or just the top one?
+    # for u,v,edgedata in graph.near_graph.edges(data=True):
+    #     # print('near-match edges ', u, v, edgedata)
+    #     label = str('{:3.2f}'.format(edgedata['weight']))
+    #     a.edge(mapping[u], mapping[v], style='dashed', label=label)
+    # # Add rank='same' information
+    # for key, value in ranking.byRank.items():
+    #     # print(key, value)
+    #     # print(key, value, len(value))
+    #     # print(key, set(value), len(set(value)))
+    #     tmp = graphviz.Digraph(graph_attr={'rank': 'same'})
+    #     for n in [mapping[item] for item in value]:
+    #         tmp.node(n)
+    #     a.subgraph(tmp)
+    # # diagnostic, not for production
+    # # dot = a.draw(prog='dot')
+    # # print(dot.decode(encoding='utf-8'))
+    # # # display using the IPython SVG module
+    # return a.render()
