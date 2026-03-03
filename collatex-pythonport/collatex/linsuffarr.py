aa32cab6caa7da9ab63e3d6e3d3c9993415f91f6
David Flood <69060117+d-flood@users.noreply.github.com>
2026-03-02 19:11:56 -0500

    update linsuffarr.py to use getfullargspec and bump collatex version to 2.4 in uv.lock

--- collatex-pythonport/collatex/linsuffarr.py
+++ collatex-pythonport/collatex/linsuffarr.py
@@ -31,7 +31,7 @@ from pickle import HIGHEST_PROTOCOL as _HIGHEST_PROTOCOL
 from pickle import dumps  as _dumps
 from pickle import loads  as _loads
 from gzip import GzipFile
-from inspect import getfullargspec as getargspec # Not yet in official release
+from inspect import getfullargspec
 from optparse import OptionParser
 from os.path import getsize
 from sys import argv   as _argv
@@ -571,7 +571,7 @@ class SuffixArray(object):
             featureName = name
 
         if arguments is None:
-            signature = getargspec(callback)[0]
+            signature = getfullargspec(callback).args
         else:
             signature = arguments
 
